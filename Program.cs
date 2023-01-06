18a804dbfe7072d67541e76b2a3726cb374232a3
DilshodDeveloper-uz <dilshodsatimov990@mail.ru>
2023-01-06 17:52:37 +0500

    Switch vazifasi

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿System.Console.WriteLine("Ikkita butun son kiriting: ");
+System.Console.Write("birinchi son: ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("ikkinchi son: ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Ikkita son orasida bajariladigan operatorni (+,-,*,/,%) kiriting:");
+string operation = Console.ReadLine();
+
+switch (operation)
+{
+    case "+":
+        System.Console.WriteLine($"{firstnumber} + {secondnumber} = {firstnumber + secondnumber}");
+        break;
+    case "-":
+        System.Console.WriteLine($"{firstnumber} - {secondnumber} = {firstnumber - secondnumber}");
+        break;
+    case "*":
+        System.Console.WriteLine($"{firstnumber} * {secondnumber} = {firstnumber * secondnumber}");
+        break;
+    case "/":
+        System.Console.WriteLine($"{firstnumber} / {secondnumber} = {(double)firstnumber / (double)secondnumber}");
+        break;
+    case "%":
+        System.Console.WriteLine($"{firstnumber} % {secondnumber} = {firstnumber % secondnumber}");
+        break;
+    default:
+    System.Console.WriteLine("Operator topilmadi!!!");
+    break;
+}
+
+
+Console.ReadKey();
+
